b18d8f5bd0b7f2747ad59fec5089b97c43de3311
mattashii <boekewurm@gmail.com>
2015-01-21 16:38:42 +0100

    Smart Computer plays better than you will ever play

--- src/mmeent/java/main/connection/player/ComputerPlayerSmart.java
+++ src/mmeent/java/main/connection/player/ComputerPlayerSmart.java
@@ -28,23 +28,49 @@ public class ComputerPlayerSmart implements Player {
         Board boardCopy = this.getGame().getBoard().deepCopy();
         List<Byte> players = new ArrayList<Byte>(game.getPlayers().keySet());
         List<Integer> rows = this.getGame().getBoard().availableRows();
-        Map<Integer,Map.Entry<Integer,Integer>> options = new HashMap<Integer,Map.Entry<Integer,Integer>>();
-        for(int i = 0; i < rows.size(); i ++) {
+        Map<Integer, WinLoss> options = new HashMap<Integer, WinLoss>();
+        for(int i = 0; i < rows.size(); i++) {
             options.put(i,determineMove(players, turn, boardCopy, searchDepth));
         }
-
-        int bestOption = 0;
+        int best = 0;
+        for(int i = 0; i < rows.size(); i++) {
+            if(options.get(i).betterThan(options.get(best))) best = i;
+        }
+        return new Move(this.game.getPlayers().get(this.id), (short) best, turn);
     }
 
-    public Map.Entry<Integer,Integer> determineMove(List<Byte> players, int turn,Board board, int depth) {
+    public WinLoss determineMove(List<Byte> players, int turn, Board board, int depth) {
         Board boardCopy = board.deepCopy();
+        WinLoss wl = new WinLoss(0, 0);
         for(int i = 0; i < board.getWidth(); i ++) {
             Move move = new Move(players.get(turn % players.size()), (short) i, turn, boardCopy);
-            if(move.isValid()) move.makeMove();
-            if(board.hasWinner()){
-                if(board.getWinner() == players.get(turn % players.size())) return new HashSet<Integer, Integer>();
-                return new HashSet<Integer, Integer>(0, 1);
+            if(!move.isValid()) continue;
+            move.makeMove();
+            if(boardCopy.hasWinner()){
+                if(boardCopy.getWinner() == players.get(turn % players.size())) wl = wl.add(new WinLoss(1, 0));
+                wl = wl.add(new WinLoss(0, 1));
+            } else if(depth > 0){
+                wl = wl.add(determineMove(players, ++turn, boardCopy, --depth));
             }
+            boardCopy = board.deepCopy();
+        }
+        return wl;
+    }
+
+    private class WinLoss{
+        public final int win;
+        public final int loss;
+        public WinLoss(int win, int loss){
+            this.win = win;
+            this.loss = loss;
+        }
+
+        public WinLoss add(WinLoss w){
+            return new WinLoss(this.win + w.win, this.loss + w.loss);
+        }
+
+        public boolean betterThan(WinLoss w){
+            return this.loss <= w.loss && this.win >= w.win;
         }
     }
 
